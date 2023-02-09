2e346077e449c4abb1ffb255c2a82874ef188824
Dung Quoc <97782511+dungnq06@users.noreply.github.com>
2023-02-09 14:19:01 +0700

    Update Lab3_1.java

--- Lab3_1.java
+++ Lab3_1.java
@@ -1,1 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lab3_1;
+
+/**
+ *
+ * @author MANH
+ */
+public class Lab3_1 {
+
+    public static void main(String[] args) {
+        int a = 8;
+        System.out.println("Hello World!");
+        Book _book = new OnlineBook("Hello", "NXB GD", "bbb", 2021, "Nam");
+        _book.showInfo();
+        
+    }
+}
 
